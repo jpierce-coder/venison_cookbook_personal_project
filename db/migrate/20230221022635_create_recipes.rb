3f993a0dc417c46f45e23c50c5cabd381aae5404
John Pierce <jpierce.coder@gmail.com>
2023-02-20 20:28:21 -0600

    Generate Rails Scaffold Files

---
+++ db/migrate/20230221022635_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :description
+      t.string :ingredients
+      t.string :instructions
+
+      t.timestamps
+    end
+  end
+end
